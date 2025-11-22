c08fd4563b1db8f469240ff1131a4cbecabea929
sandeepkumar1209 <59911291+sandeepkumar1209@users.noreply.github.com>
2025-11-22 23:01:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ def index():
     return "Welcome to Python Flask world v1.0"
 
 if __name__ == '__main__':
-    app.run('host=0.0.0.0', port=8080)
-
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
 
